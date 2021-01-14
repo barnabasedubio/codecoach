d061ab0a7fa0f06ea16938ee49a352305dcf0a49
Barnabas Edubio <barny.edubio@gmail.com>
2021-01-14 21:51:42 +0100

    add new homepage component

---
+++ src/components/TheHome.vue
@@ -0,0 +1,27 @@
+<template>
+	<main>
+		<the-preferences></the-preferences>
+		<coach-list></coach-list>
+	</main>
+</template>
+
+<script>
+import ThePreferences from "./ThePreferences.vue";
+import CoachList from "./coaches/CoachList.vue";
+export default {
+	components: {
+		ThePreferences,
+		CoachList
+	}
+};
+</script>
+
+<style>
+main {
+	border: 1px solid red;
+	min-height: 90vh;
+	width: 60%;
+	margin: 0 auto;
+	padding: 1rem;
+}
+</style>
