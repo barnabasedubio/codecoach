50229eb9ef9cc1e66e68732de0a3cc9402bb0fbd
Barnabas Edubio <barny.edubio@gmail.com>
2021-01-14 21:52:15 +0100

    add testdata

---
+++ src/data/coachList.js
@@ -0,0 +1,68 @@
+import generateID from "./generateID.js";
+
+export default [
+	{
+		id: generateID(),
+		name: "Tom Hardy",
+		languages: ["Java", "PHP"],
+		salary: 60,
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e"
+	},
+	{
+		id: generateID(),
+		name: "Jessica Alba",
+		languages: ["Python"],
+		salary: 25,
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e"
+	},
+	{
+		id: generateID(),
+		name: "Nina Dobrev",
+		languages: ["Python", "JavaScript"],
+		salary: 120,
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e"
+	},
+	{
+		id: generateID(),
+		name: "Jermaine Jones",
+		languages: ["C#", "PHP", "Python"],
+		salary: 35,
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e"
+	},
+	{
+		id: generateID(),
+		name: "Peter Jackson",
+		languages: ["JavaScript", "Java", "C#"],
+		salary: 22,
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e"
+	},
+	{
+		id: generateID(),
+		name: "Mark Zuckerberg",
+		languages: ["C#", "PHP"],
+		salary: 37,
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e"
+	},
+	{
+		id: generateID(),
+		name: "Elon Musk",
+		languages: ["C#"],
+		salary: 1200,
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e"
+	},
+	{
+		id: generateID(),
+		name: "Jeff Bezos",
+		languages: ["Python", "PHP", "Java", "JavaScript"],
+		salary: 750,
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e"
+	}
+];
