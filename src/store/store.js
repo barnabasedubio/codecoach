352cd3ad05a6157833a33609e6c784307d1fc4ca
Barnabas Edubio <barny.edubio@gmail.com>
2021-01-14 22:38:04 +0100

    removed unnecessary module

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,11 @@
 import { createStore } from "vuex";
 import authentication from "./modules/authentication.js";
-import preferences from "./modules/preferences.js";
 import requests from "./modules/requests.js";
 import coaches from "./modules/coaches.js";
 
 const store = createStore({
 	modules: {
 		authentication,
-		preferences,
 		requests,
 		coaches
 	}
